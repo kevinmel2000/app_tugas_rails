185e5f28135ea7a0330a4805ebf67e7e96156f86
fathan <kreasi.fathan@gmail.com>
2015-01-12 13:00:29 +0700

    revision UI select box searching product car israj

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -44,7 +44,7 @@ module ApplicationHelper
     @transmission = ['manual','automatic']
   end
   def seat
-    @seat = ['2','4','6']
+    @seat = ['1','2','3','4','5','6','7','8','9','10','11','12']
   end
   def door
     @door = ['2','4','5']
